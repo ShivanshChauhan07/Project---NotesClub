0db6f57a2eb7292bbf92a3d3383901ea6e441fcc
rohitmauryax <rohitmaurya152@gmail.com>
2023-11-17 20:21:19 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
-import Unversities from "./components/Unversities";
+import { Home } from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Header />
-      <Body />
-      <Unversities />
-      <Footer />
+      <Home />
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Body from "./Body";
+import Headers from "./Header";
+import Unversities from "./Unversities";
+import Footer from "./Footer";
+export const Home = () => {
+  return (
+    <div>
+      <Headers />
+      <Body />
+      <Unversities />
+      <Footer />
+    </div>
+  );
+};
