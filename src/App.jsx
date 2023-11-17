7bdc0e73989fbd956e427780f625ef2a49a3578f
rohitmauryax <rohitmaurya152@gmail.com>
2023-11-17 20:24:31 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Notes } from "./components/Notes";
 import { Home } from "./components/Home";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/notes/:name" element={<Notes />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
