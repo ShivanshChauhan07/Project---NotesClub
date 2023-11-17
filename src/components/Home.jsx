e2f48dd59e454e34dcc32bac8d05e9e18b2d39e6
rohitmauryax <rohitmaurya152@gmail.com>
2023-11-17 20:27:06 +0530

    Fourth

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Body from "./Body";
 import Headers from "./Header";
-import Unversities from "./Unversities";
-import Footer from "./Footer";
 export const Home = () => {
   return (
     <div>
       <Headers />
       <Body />
-      <Unversities />
-      <Footer />
     </div>
   );
 };
